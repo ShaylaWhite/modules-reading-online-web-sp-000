c24036512aa2ee784ef0985180f18b147890868a
Shayla White <shayladwhite1@gmail.com>
2019-08-04 17:29:21 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,5 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-clasinclude Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  ends Dancer
+class Dancer
   
 end
